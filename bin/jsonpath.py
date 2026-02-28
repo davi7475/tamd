3e133d3592978b444ef17513feaaee66cc2c27c5
davi7475 <149542367+davi7475@users.noreply.github.com>
2026-02-27 21:23:36 -0300

    Delete bin directory

--- bin/jsonpath.py
+++
@@ -1,6 +0,0 @@
-#!/home/davi/YAMD/.venv/bin/python
-import sys
-from jsonpath_rw.bin.jsonpath import entry_point
-if __name__ == '__main__':
-    sys.argv[0] = sys.argv[0].removesuffix('.exe')
-    sys.exit(entry_point())
